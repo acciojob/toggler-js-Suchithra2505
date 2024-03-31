2733caa6a6fe85a4e947cc04590e06ef23e8f57c
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2024-03-31 18:54:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+const checkboxes = document.querySelectorAll('.toggle input[type="checkbox"]');
+
+    checkboxes.forEach(checkbox => {
+      checkbox.addEventListener('change', () => {
+        let checkedCount = 0;
+        checkboxes.forEach(cb => {
+          if (cb.checked) checkedCount++;
+        });
+
+        if (checkedCount > 2) {
+          checkboxes.forEach(cb => {
+            if (!cb.checked) cb.disabled = true;
+          });
+        } else {
+          checkboxes.forEach(cb => {
+            cb.disabled = false;
+          });
+        }
+      });
+    });
